e000d10644dfaf4766ba28aa2c93d0634a840833
caioRafael <caiorafaelrg@gmail.com>
2022-11-17 23:16:49 -0300

    front

--- anotations-front/src/components/SideBar.tsx
+++ anotations-front/src/components/SideBar.tsx
@@ -1,6 +1,8 @@
 import { GridItem, Button, Text } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function SideBar(){
+    const [selected, setSelected] = useState<boolean>(false)
     return(
         <GridItem p='2' borderRadius={5} bg='#1E1E1E' area={'nav'}>
             <Button 
@@ -8,8 +10,31 @@ function SideBar(){
                 borderRadius={5} 
                 display={"flex"} 
                 alignItems={'center'}
+                border={"2px"}
+                borderColor={selected ? "teal" : '#0F0F0F'}
+                onClick={() => setSelected(!selected)}
+                _hover={{ bg: '#1E1E1E' }}
+                marginBottom={3}
             >
-                <Text color='teal' fontSize={'2xl'}>3</Text>
+                <Text color='teal' fontSize={'2xl'}>
+                    Anotações
+                </Text>
+            </Button>
+            
+            <Button 
+                w={'100%'} h={55} bg={'#0F0F0F'} 
+                borderRadius={5} 
+                display={"flex"} 
+                alignItems={'center'}
+                border={"2px"}
+                borderColor={selected ? "teal" : '#0F0F0F'}
+                onClick={() => setSelected(!selected)}
+                _hover={{ bg: '#1E1E1E' }}
+                marginBottom={3}
+            >
+                <Text color='teal' fontSize={'2xl'}>
+                    Anotações
+                </Text>
             </Button>
         </GridItem>
     )
